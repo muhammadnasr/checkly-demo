99a0d202beeb5a156932ffa61ec19826352ab9c6
muhammadnasr <muhammad.hassan.nasr@gmail.com>
2024-03-21 07:16:43 +0300

    make it sucess

--- __checks__/about.spec.ts
+++ __checks__/about.spec.ts
@@ -3,9 +3,9 @@ import { test, expect } from '@playwright/test'
 // You can override the default Playwright test timeout of 30s
 // test.setTimeout(60_000);
 
-test('webshop homepage', async ({ page }) => {
-  const response = await page.goto('https://muhammad.engineer')
+test('checkly homepage', async ({ page }) => {
+  const response = await page.goto('https://www.checklyhq.com')
   expect(response?.status()).toBeLessThan(400)
-  await expect(page).toHaveTitle(/Muhammad Nasr2/)
+  await expect(page).toHaveTitle(/Code, Test & Deploy/)
   await page.screenshot({ path: 'homepage.jpg' })
 })
