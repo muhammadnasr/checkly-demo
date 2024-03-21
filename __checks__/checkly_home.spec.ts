a7602c3d97de6281df663cb713a1fffb4567514e
muhammadnasr <muhammad.hassan.nasr@gmail.com>
2024-03-21 07:54:09 +0300

    make it

---
+++ __checks__/checkly_home.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test'
+
+// You can override the default Playwright test timeout of 30s
+// test.setTimeout(60_000);
+
+test('checkly homepage', async ({ page }) => {
+  const response = await page.goto('https://www.checklyhq.com')
+  expect(response?.status()).toBeLessThan(400)
+  await expect(page).toHaveTitle(/Code, Test & Deploy/)
+  await page.screenshot({ path: 'homepage.jpg' })
+})
